e4d5c7a8c89913465a963d391f36cd1e894b7a7c
Nicolas <Nikolaskozhevin@gmail.com>
2026-09-18 12:26:50 +0300

    Initial Commit

---
+++ TimeTracker/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  TimeTracker
+//
+//  Created by Nicolas Kozhevin on 18.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
